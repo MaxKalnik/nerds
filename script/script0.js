5890a5bb96100388fe53d5b4228cc43a9ab13608
Maksim Kalnik <kalnikmaxim@gmail.com>
2017-03-14 12:49:43 +0200

    Cpmmit

---
+++ script/script0.js
@@ -0,0 +1,7 @@
+var allDiv = document.getElementByTagName("div");
+for (var i = 0; i < allDiv.length; i++) {
+allDiv[i].style.borderWidth = "1px";
+allDiv[i].style.borderStyle = "solid";
+allDiv[i].style.borderStyle = "red";
+
+}
